296643743a650b7d1eb0510856f19f9003706ac0
Jack Young <jdy2003@colostate.edu>
2024-04-26 14:00:05 -0600

    more analysis

--- sample/analysis/analysis.py
+++ sample/analysis/analysis.py
@@ -3,19 +3,22 @@ import pyspark.sql.functions as F
 from pyspark.sql.types import StructType, StructField, StringType, FloatType
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.regression import LinearRegression
+from pyspark.ml import PipelineModel
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator, RegressionEvaluator
+import matplotlib.pyplot as plt
 
 import os
 os.environ["PYSPARK_PYTHON"] = "/s/bach/j/under/jdy2003/miniconda3/bin/python3.12"
 
 spark = SparkSession.builder.appName("analysis").getOrCreate()
 
-# Load the model with the specified schema
-
+# load model and the data
 path_to_model = '/user/jdy2003/best_xgb_model'
-predictions = spark.read.csv("/user/jdy2003/predictions", header=True)
-predictions.show()
+crime_data = spark.read.csv("/user/jdy2003/nycFrame", header=True)
 
-zone_predictions = predictions.groupBy("zone") \
+crime_predictions = spark.read.csv("/user/jdy2003/predictions", header=True)
+
+zone_predictions = crime_predictions.groupBy("zone") \
     .agg(
         F.avg("prediction").alias("avg_prediction"),
         F.avg("response_time_in_minutes").alias("avg_response_time"),
@@ -27,55 +30,98 @@ best_zones = zone_predictions.orderBy(F.col("avg_prediction").asc())
 worst_zones = zone_predictions.orderBy(F.col("avg_prediction").desc())
 
 print("BEST PERFORMING ZONES")
-best_zones.show()
+best_zones.show()   
+best_zones.write.mode("overwrite").option("header", "true").csv("/user/jdy2003/bestZones")
 print("WORST PERFORMING ZONES")
 worst_zones.show() 
-
-# BELOW IS THE VISUALIZATION
-
-# predictions_pandas = predictions.select("prediction", "response_time_in_minutes").toPandas() # gets the fields we want
-# plt.figure(figsize=(8, 6)) # 8 inches by 6 inches yo
-# plt.scatter(predictions_pandas["prediction"], predictions_pandas["response_time_in_minutes"], alpha=0.5) # alpha = .5 is semitransparent markers
-# plt.xlabel("Predicted Response Time (minutes)")
-# plt.ylabel("Actual Response Time (minutes)")
-# plt.title("Scatter Plot: Predicted vs Actual Response Time")
-
-# temp_dir = "/s/bach/j/under/jdy2003/scatter/scatter_plot.png"
-# plot_path = os.path.join(temp_dir, "scatter_plot.png")
-# plt.savefig(plot_path, format='png')
-
-# # Print the path to the saved plot
-# print(f"Scatter plot saved at: {plot_path}")
-
-#  that shit above takes FOREVER to run lol
-
-# BELOW IS SEGMENTATION ANALYSIS
-segmented_analysis = predictions.groupBy("TYP_DESC").agg(F.avg("prediction").alias("avg_prediction"))
-segmented_analysis.show()
+worst_zones.write.mode("overwrite").option("header", "true").csv("/user/jdy2003/bestZones")
 
 
-# BELOW IS FEATURE IMPORTANCE / CORRELATION
 
+# BELOW IS THE AREA FOR METRICS MAE, R^2 RMSE
+evaluator_rmse = RegressionEvaluator(labelCol="response_time_in_minutes", predictionCol="prediction", metricName="rmse")
+evaluator_mae = RegressionEvaluator(labelCol="response_time_in_minutes", predictionCol="prediction", metricName="mae")
+evaluator_r2 = RegressionEvaluator(labelCol="response_time_in_minutes", predictionCol="prediction", metricName="r2")
+
+crime_predictions = crime_predictions.withColumn("prediction", F.col("prediction").cast("float"))
+crime_predictions = crime_predictions.withColumn("response_time_in_minutes", F.col("response_time_in_minutes").cast("float"))
+
+
+rmse = evaluator_rmse.evaluate(crime_predictions)
+mae = evaluator_mae.evaluate(crime_predictions)
+r2 = evaluator_r2.evaluate(crime_predictions)
+
+metric_path = "/s/bach/j/under/jdy2003/metrics.txt"
+
+with open(metric_path, 'w') as file:
+    file.write(f"{"Root Mean Squared Error (RMSE):"} {rmse} \n")
+    file.write(f"{"Mean Average Error (MAE): "} {mae} \n")
+    file.write(f"{"R Squared Value: "} {r2} \n")
+
+print("Metrics written to file at ", metric_path)
+# # BELOW IS FEATURE IMPORTANCE / CORRELATION
 cast_columns = ['TAVG', 'PRCP', 'event_type_value', 'zone', 'DayOfYear',
                 'response_time_in_minutes', 'Latitude', 'Longitude', 'accident_count']
 for column in cast_columns:
-    predictions = predictions.withColumn(column, F.col(column).cast("float"))
+    crime_predictions = crime_predictions.withColumn(column, F.col(column).cast("float"))
 
 assembler = VectorAssembler(inputCols=["zone", "TAVG", "PRCP", "accident_count", "event_type_value"],
                             outputCol="features")
 
-predictions = assembler.transform(predictions)
+crime_predictions = assembler.transform(crime_predictions)
 
 lr = LinearRegression(featuresCol="features", labelCol="response_time_in_minutes")
 
-predictions = predictions.drop(F.col("prediction"))
-model = lr.fit(predictions)
+coefficients = crime_predictions.drop(F.col("prediction"))
+model = lr.fit(coefficients)
 
 print("Feature Coefficients:")
 coefficients = model.coefficients
-for i, col in enumerate(assembler.getInputCols()):
-    print(f"{col}: {coefficients[i]}")
+
+file_path = "/s/bach/j/under/jdy2003/correlation.txt"
+
+with open(file_path, 'w') as file:
+    file.write("Feature Coefficients:\n")
+    
+    for i, col in enumerate(assembler.getInputCols()):
+        file.write(f"{col}: {coefficients[i]}\n")
+
+print("Feature coefficients have been written to ", file_path)
 
 
+
+# BELOW IS RESIDUAL ANALYSIS
+crime_predictions = crime_predictions.withColumn("residuals", col("response_time_in_minutes") - col("prediction"))
+residual_panda = crime_predictions.select("residuals").toPandas()
+plt.figure(figsize=(8, 6))
+plt.scatter(range(len(residual_panda)), residual_panda["residuals"], alpha=0.5)
+plt.xlabel("Index")
+plt.ylabel("Residuals")
+residual_dir = "/s/bach/j/under/jdy2003/images/residual.png"
+plt.savefig(residual_dir,format="png")
+
+
+#BELOW IS TIME SERIES ANALYSIS
+time_series = crime_predictions.groupBy("DayOfYear").agg(F.avg("response_time_in_minutes").alias('avg_response_time'))
+time_series_panda = time_series.toPandas()
+plt.figure(figsize=(8,6))
+plt.plot(time_series_panda["DayOfYear"],time_series_panda["avg_response_time"])
+plt.xlabel("Day of Year")
+plt.ylabel("Average Response Time (minutes)")
+plt.title("Average Response Time Over Time")
+time_series_dir = "/s/bach/j/under/jdy2003/images/time_series.png"
+plt.savefig(time_series_dir, format="png")
+
+# BELOW IS THE PREDICTION PLOT
+predictions_pandas = crime_predictions.select("prediction", "response_time_in_minutes").toPandas() # gets the fields we want
+plt.figure(figsize=(8, 6)) # 8 inches by 6 inches yo
+plt.scatter(predictions_pandas["prediction"], predictions_pandas["response_time_in_minutes"], alpha=0.5) # alpha = .5 is semitransparent markers
+plt.xlabel("Predicted Response Time (minutes)")
+plt.ylabel("Actual Response Time (minutes)")
+plt.title("Scatter Plot: Predicted vs Actual Response Time")
+
+predictions_path = "/s/bach/j/under/jdy2003/images/scatter_plot.png"
+plot_path = os.path.join(predictions_path, "scatter_plot.png")
+plt.savefig(plot_path, format='png')
 
 spark.stop()
--- sample/train.py
+++ sample/train.py
@@ -85,7 +85,7 @@ r2 = evaluator_r2.evaluate(predictions)
 
 best_model = cvModel.bestModel
 best_model_path = "/user/jdy2003/best_gbt_model"
-predictions_path = "/user/jdy2003/predictions1"
+predictions_path = "/user/jdy2003/predictions"
 
 predictions.show(20)
 
@@ -109,50 +109,4 @@ print("Root Mean Squared Error (RMSE):", rmse)
 print("Mean Absolute Error (MAE):", mae)
 print("R-squared (R^2) Value:", r2)
 # Stop Spark session
-spark.stop()
-
-# xgb = GBTRegressor(featuresCol="features", labelCol="response_time_in_minutes")
-
-# pipeline = Pipeline(stages=[assembler, xgb])
-
-# # below builds a parameter grid to search the hyperparam space for optimal combinations
-# paramGrid = ParamGridBuilder() \
-#     .addGrid(xgb.maxDepth, [5, 10]) \
-#     .addGrid(xgb.maxBins, [20, 40]) \
-#     .build()
-
-# evaluator = RegressionEvaluator(labelCol="response_time_in_minutes", predictionCol="prediction", metricName="rmse")
-
-# crossval = CrossValidator(estimator=pipeline,
-#                           estimatorParamMaps=paramGrid,
-#                           evaluator=evaluator,
-#                           numFolds=5)  # Number of folds for cross-validation
-
-# cvModel = crossval.fit(crime_data)
-
-# evaluator_r2 = RegressionEvaluator(labelCol="response_time_in_minutes", predictionCol="prediction", metricName="r2")
-# evaluator_mae = RegressionEvaluator(labelCol="response_time_in_minutes", predictionCol="prediction", metricName="mae")
-
-# predictions = cvModel.transform(crime_data)
-# rmse = evaluator.evaluate(predictions)
-# r2 = evaluator_r2.evaluate(predictions)
-# mae = evaluator_mae.evaluate(predictions)
-
-# print("Root Mean Squared Error (RMSE):", rmse)
-
-# best_model = cvModel.bestModel.stages[-1]  #  that grabs the best model found by the cross val and param grid
-
-# best_model_path = "/user/jdy2003/best_xgb_model"
-# predictions.show(10)
-
-# predictions = predictions.drop(col("features"))
-# predictions.show(10)
-
-# predictions.write.format("csv").option("header", "true").mode("overwrite").save("/user/jdy2003/predictions")
-
-# print("Root Mean Squared Error (RMSE):", rmse)
-# print("R squared value: ", r2)
-# print("MAE score ", mae)
-
-
 spark.stop()
